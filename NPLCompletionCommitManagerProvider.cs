87b0e61dae512689cc26f9b0decd91f1847cdda4
lixizhi <lixizhi@yeah.net>
2023-05-02 15:09:03 +0800

    new code completion interface sample added

---
+++ NPLCompletionCommitManagerProvider.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.Language.Intellisense.AsyncCompletion;
+using Microsoft.VisualStudio.Text.Editor;
+using Microsoft.VisualStudio.Utilities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NPLForVisualStudio
+{
+    [Export(typeof(IAsyncCompletionCommitManagerProvider))]
+    [Name("NPL/Lua language commit manager provider")]
+    [ContentType("text")]
+    class NPLCompletionCommitManagerProvider : IAsyncCompletionCommitManagerProvider
+    {
+        IDictionary<ITextView, IAsyncCompletionCommitManager> cache = new Dictionary<ITextView, IAsyncCompletionCommitManager>();
+
+        public IAsyncCompletionCommitManager GetOrCreate(ITextView textView)
+        {
+            if (cache.TryGetValue(textView, out var itemSource))
+                return itemSource;
+
+            var manager = new NPLCompletionCommitManager();
+            textView.Closed += (o, e) => cache.Remove(textView); // clean up memory as files are closed
+            cache.Add(textView, manager);
+            return manager;
+        }
+    }
+}
